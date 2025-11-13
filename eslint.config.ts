957fec09cca984bcc307176d9c10e43d251eb1f5
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-13 01:03:58 +0100

    chore: setup eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.angular/**',
+      '**/coverage/**',
+      '**/.expo/**',
+      '**/android/**',
+      '**/ios/**',
+      '.husky/**',
+      '**/eslint.config.ts',
+   ],
+  },
+);
---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+};
+
+export default config;
