5a3cbb40ea320c920b955a0c3d707d8786e3a745
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-13 01:01:43 +0100

    chore(backend): setup eslint and prettier

---
+++ backend/eslint.config.ts
@@ -0,0 +1,24 @@
+import baseConfig from '../eslint.config';
+import tseslint from 'typescript-eslint';
+
+export default [  
+  ...baseConfig,
+  ...tseslint.configs.strict,
+  
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
