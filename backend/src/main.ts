fd259cfa2195f8e0e5226298db1fa2d053f48da7
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-18 15:04:29 +0100

    chore(backend): add @nestjs/swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('GeoAware Proxy')
+    .setDescription('The GeoAware Proxy API description')
+    .setVersion('1.0')
+    .addTag('geoproxy')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
