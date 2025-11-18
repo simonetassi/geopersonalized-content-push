f882de5bdf4b8811f894395afb74ea116d496d58
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-18 13:08:52 +0100

    feat(backend-setup): setup TypeOrmModule and AppModule using env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GeofenceModule } from './geofences/geofence.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST', 'localhost'),
+        port: configService.get<number>('POSTGRES_PORT', 5432),
+        username: configService.get<string>('POSTGRES_USER', 'postgres'),
+        password: configService.get<string>('POSTGRES_PASSWORD', 'postgres'),
+        database: configService.get<string>('POSTGRES_DB', 'app'),
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        logging: configService.get<string>('NODE_ENV') === 'development',
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    GeofenceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
