f02400d62e0fc3b62b1110c6770d6fc55adf9d3a
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-24 00:15:57 +0100

    feat(back-events): add EventController

---
+++ backend/src/events/events.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDTO, EventDTO } from './dto';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { FindEventsDTO } from './dto/find-events.dto';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  @ApiOperation({ operationId: 'create' })
+  @ApiOkResponse({ type: [EventDTO] })
+  public create(@Body() createEventDTO: CreateEventDTO): Promise<EventDTO> {
+    return this.eventsService.create(createEventDTO);
+  }
+
+  @Get()
+  @ApiOperation({ operationId: 'list' })
+  @ApiOkResponse({ type: [EventDTO] })
+  findAll(@Query() query: FindEventsDTO) {
+    return this.eventsService.findAll(query);
+  }
+}
