789f25d1c4e597618e087de2d8c3447198338d2a
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-11-24 00:14:38 +0100

    feat(back-events): add EventEntity

---
+++ backend/src/events/entities/event.entity.ts
@@ -0,0 +1,42 @@
+import { Geofence } from '@/geofences/entities/geofence.entity';
+import { User } from '@/users/entities/user.entity';
+import { Point } from 'geojson';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { EventType } from '../common/event-type';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'enum', enum: EventType })
+  type: EventType;
+
+  @ManyToOne(() => Geofence, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'fenceId' })
+  fence: Geofence;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column({
+    type: 'geometry',
+    srid: 4326,
+    spatialFeatureType: 'Point',
+  })
+  location: Point;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  timestamp: Date;
+}
