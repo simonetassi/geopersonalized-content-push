28dff982d7bf3010f4ede119992a204c0526419b
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-01-02 17:02:30 +0100

    feat(front-map): add GeofencesPanelComponent

---
+++ frontend/src/app/pages/dashboard/components/geofences-panel/geofences-panel.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Geofence } from '@/common/interfaces';
+
+@Component({
+  selector: 'app-geofence-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './geofences-panel.component.html',
+  styleUrl: './geofences-panel.component.scss',
+})
+export class GeofencePanelComponent {
+  @Input()
+  public geofences: Geofence[] = [];
+  @Input()
+  public selectedId?: string;
+
+  @Output()
+  public selected = new EventEmitter<Geofence>();
+  @Output()
+  public created = new EventEmitter<void>();
+  @Output()
+  public searched = new EventEmitter<string>();
+
+  public isCollapsed: boolean = false;
+
+  public selectFence(fence: Geofence): void {
+    this.selected.emit(fence);
+  }
+
+  public createNew(): void {
+    this.created.emit();
+  }
+
+  public handleInput(event: Event): void {
+    const term = (event.target as HTMLInputElement).value;
+    this.searched.emit(term);
+  }
+
+  public toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
