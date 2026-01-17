26b3bb4c3c2d7e1bca994d411e826de3b57dfbe3
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-01-17 18:14:13 +0100

    feat(app-history): add ContentMeta to interfaces

--- mobile-app/src/interfaces/geofence.interface.ts
+++ mobile-app/src/interfaces/geofence.interface.ts
@@ -1,8 +1,10 @@
 import { Geometry } from 'geojson';
+import { ContentMeta } from './content-meta.interface';
 
 export interface Geofence {
   id: string;
   name: string;
   geometry: Geometry;
   metadata: Record<string, unknown>;
+  contents: ContentMeta[];
 }
