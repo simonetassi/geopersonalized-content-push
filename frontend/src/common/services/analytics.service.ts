178d5dba840db03f4477148c5324df9bb4c992b3
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-01-26 22:54:01 +0100

    feat(front-heatmap): add AnalyticsService

---
+++ frontend/src/common/services/analytics.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@/environments/environment';
+import { Observable } from 'rxjs';
+import { FeatureCollection } from 'geojson';
+
+@Injectable({ providedIn: 'root' })
+export class AnalyticsService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/analytics`;
+
+  public getHeatmapData(): Observable<FeatureCollection> {
+    return this.http.get<FeatureCollection>(`${this.apiUrl}/heatmap`);
+  }
+}
