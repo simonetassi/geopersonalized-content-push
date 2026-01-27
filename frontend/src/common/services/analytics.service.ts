1c17400db277cda320b5c846c0acf61ed1b1d7df
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-01-27 20:32:22 +0100

    feat(front-clustering): get clustering data in AnalyticsService

--- frontend/src/common/services/analytics.service.ts
+++ frontend/src/common/services/analytics.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@/environments/environment';
 import { Observable } from 'rxjs';
 import { FeatureCollection } from 'geojson';
+import { ClusteringMetrics } from '../interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class AnalyticsService {
@@ -11,5 +12,9 @@ export class AnalyticsService {
 
   public getHeatmapData(): Observable<FeatureCollection> {
     return this.http.get<FeatureCollection>(`${this.apiUrl}/heatmap`);
+  }
+
+  public getClusteringData(): Observable<ClusteringMetrics[]> {
+    return this.http.get<ClusteringMetrics[]>(`${this.apiUrl}/clustering`);
   }
 }
