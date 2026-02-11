fd0b60806255b5849e741375ae00a1d1f724cd59
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-02-11 18:10:36 +0100

    feat: remove host name check from ingress config

--- mobile-app/src/api/client.ts
+++ mobile-app/src/api/client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.1.248:3000';
+// const API_URL = 'http://192.168.1.248:3000'; // for local execution
+const API_URL = 'http://192.168.49.2/api'; // for minikube execution
 
 export const api = axios.create({
   baseURL: API_URL,
